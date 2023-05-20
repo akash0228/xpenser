c8cd675f2dccd010fc102011972f8f42c9ae99bc
Akash <ankieekumar@gmail.com>
2023-05-20 08:37:34 +0530

    header done

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div className='header-container'>
+        <div className="header">
+            <div className='header-logo'>Xpensr<i class="fi fi-rr-credit-card"></i></div>
+            <div className="header-button">
+                <a href='https://github.com' target='_blank' rel='noopener noreferrer'>
+                    <i class="devicon-github-original"></i>Star
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
